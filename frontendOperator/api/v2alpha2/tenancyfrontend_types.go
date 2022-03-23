328d671eac0944f070152839ced60d539e78a0cd
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-03-23 15:13:21 +0100

    update with +kubebuilder:storageversion

--- frontendOperator/api/v2alpha2/tenancyfrontend_types.go
+++ frontendOperator/api/v2alpha2/tenancyfrontend_types.go
@@ -46,8 +46,9 @@ type TenancyFrontendStatus struct {
 	// Important: Run "make" to regenerate code after modifying this file
 }
 
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:storageversion
 
 // TenancyFrontend is the Schema for the tenancyfrontends API
 type TenancyFrontend struct {
