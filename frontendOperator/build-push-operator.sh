6250e236ec0f5020d1f32b4b3ba95b56c1a292a3
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-03-23 20:53:39 +0100

    bash script to create and push images

---
+++ frontendOperator/build-push-operator.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "-> Build contoller manager image"
+echo ""
+
+export VERSION=0.0.2
+export REGISTRY='quay.io'
+export ORG='tsuedbroecker'
+export CONTROLLER_IMAGE='frontendcontroller:v4'
+
+echo ""
+echo "-> build"
+echo "-> $REGISTRY/$ORG/$CONTROLLER_IMAGE"
+echo ""
+make generate
+make manifests
+make docker-build IMG="$REGISTRY/$ORG/$CONTROLLER_IMAGE"
+
+echo ""
+echo "-> push"
+echo ""
+docker push "$REGISTRY/$ORG/$CONTROLLER_IMAGE"
+
+echo ""
+echo "-> create bundle"
+make bundle IMG="$REGISTRY/$ORG/$CONTROLLER_IMAGE"
+echo ""
+
+echo ""
+echo "-> build bundle"
+export BUNDLE_IMAGE='bundlefrontendoperator:v4'
+make bundle-build BUNDLE_IMG="$REGISTRY/$ORG/$BUNDLE_IMAGE"
+echo ""
+
+echo ""
+echo "-> push bundle"
+docker push "$REGISTRY/$ORG/$BUNDLE_IMAGE"
+echo ""
+
+
+echo ""
+echo "-> build catalog"
+export CATALOG_IMAGE=frontend-catalog
+export CATALOG_TAG=v0.0.2
+make catalog-build CATALOG_IMG="$REGISTRY/$ORG/$CATALOG_IMAGE:$CATALOG_TAG" BUNDLE_IMGS="$REGISTRY/$ORG/$BUNDLE_IMAGE"
+echo ""
+
+echo ""
+echo "-> push catalog"
+docker push "$REGISTRY/$ORG/$CATALOG_IMAGE:$CATALOG_TAG"
+echo ""
+
+kubectl apply -f "olm-configuration/catalogsource.yaml" -n operators
+kubectl apply -f "olm-configuration/subscription.yaml" -n operators
