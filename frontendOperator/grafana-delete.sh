a95718f6332b283bc67bfb19f47b3b17d28dd920
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-04-26 11:27:27 +0200

    update grafana delete srcipt

--- frontendOperator/grafana-delete.sh
+++ frontendOperator/grafana-delete.sh
@@ -2,3 +2,10 @@
 
 kubectl delete -f grafana-instance.yaml -n grafana-operator
 kubectl delete -f grafana-operator-setup.yaml -n grafana-operator
+
+kubectl get crds | greb 'grafa'
+
+kubectl delete customresourcedefinition grafanadashboards.integreatly.org
+kubectl delete customresourcedefinition grafanadatasources.integreatly.org
+kubectl delete customresourcedefinition grafananotificationchannels.integreatly.org
+kubectl delete customresourcedefinition grafanas.integreatly.org 
